75ba8b83d7f8922b7906caa3112d296df77cbd02
rahulkuma9503-coder <rahulkuma9503@gmail.com>
2025-11-13 07:48:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ADMIN_IDS = list(map(int, os.getenv('ADMIN_IDS', '').split(','))) if os.getenv('ADMIN_IDS') else []
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
